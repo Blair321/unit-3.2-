b63a86f98dce34f86bdea8bb2933eed130db6dff
Blair Jilek <blairjilek@gmail.com>
2024-10-10 20:46:33 -0500

    made loop for stars with for of loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -35,14 +35,16 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
-
+for (galaxy of stars){console.log(stars);
+}
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+while (condition) {
+  
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
